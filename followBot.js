74362405354b2faea9ce53a257e7dc0cc6029230
Daniel Russell <contactdanielrussell@gmail.com>
2021-03-25 23:08:13 -0500

    Update followBot.js

--- followBot.js
+++ followBot.js
@@ -1,9 +1,7 @@
 (() => {
     let i = 0;
     const followInterval = setInterval(() => {
-        if (i >= 30) {
-            clearInterval(followInterval);
-        }
+        if (i >= 30) clearInterval(followInterval);
         const buttons = document.querySelectorAll('button');
         const nextButton = buttons[i];
         if (nextButton.textContent.toLowerCase() === 'follow') { // write in your language
