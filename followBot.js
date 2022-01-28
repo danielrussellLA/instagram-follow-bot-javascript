4591e3001aa5b0b6185f684f68d4d6d1a0543a76
Mohammad Noman <44547462+mohammadn0man@users.noreply.github.com>
2022-01-29 00:47:40 +0530

    final script

--- followBot.js
+++ followBot.js
@@ -5,16 +5,16 @@
         const newInterval = setInterval(() => {
             const btn = document.querySelectorAll('button');
             const nxt_btn = btn[i];
-            if (nxt_btn.textContent.toLowerCase() === 'follow') {
+            if (nxt_btn !== undefined && nxt_btn.textContent.toLowerCase() === 'follow') {
                 nxt_btn.click();
-                console.log("Followed : " + (i+1) + ". " + nxt_btn.parentElement.parentElement.childNodes[1].firstElementChild.firstElementChild.firstElementChild.firstElementChild.text);
+                console.log("Followed : " + (count+1) + ". " + nxt_btn.parentElement.parentElement.childNodes[1].firstElementChild.firstElementChild.firstElementChild.firstElementChild.text);
                 count++;
             }
             i++;
             if(count >= 30){
                 console.log("sleep for 1 hrs ");
                 count = 0;
-                i=0;
+                i = 0;
                 clearInterval(newInterval);
             }
         }, 1000)
